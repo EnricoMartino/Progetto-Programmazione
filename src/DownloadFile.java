089144dabdfde6c136b48b9cf423c5363b11921f
Luca Munerati <s1082299@studenti.univpm.it>
2019-05-31 22:29:26 +0200

    Update DownloadFile.java

--- src/DownloadFile.java
+++ src/DownloadFile.java
@@ -1,4 +1,35 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class DownloadFile {
 
+	public void downloadFileFromURL(String url, String filePathName, 
+			int readTimeoutInSeconds) 
+			throws MalformedURLException, IOException{
+				
+			//create connection, setting a timeout
+			URLConnection urlConn = new URL(url).openConnection();
+			if(readTimeoutInSeconds>0)
+				urlConn.setReadTimeout(readTimeoutInSeconds*1000);
+				
+			//create the stream
+			BufferedInputStream in=new BufferedInputStream(urlConn.getInputStream());
+			FileOutputStream fos = new FileOutputStream(filePathName);
+
+			//read the file
+			BufferedOutputStream bout = new BufferedOutputStream(fos,1024);
+			byte[] data = new byte[1024];
+			int x=0;
+			while((x=in.read(data,0,1024))>=0){
+				bout.write(data,0,x);
+			}
+			bout.close();
+			in.close();
+		}	
+	
 }
