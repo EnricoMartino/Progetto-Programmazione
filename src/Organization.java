30f434d8676125e53c2bbeb38651674103a7dcdf
Luca Munerati <s1082299@studenti.univpm.it>
2019-06-03 14:11:48 +0200

    Create Organization class
    Create organization class and create constructor. Use this class for know the organization of "casa vacanza"

---
+++ src/Organization.java
@@ -0,0 +1,20 @@
+
+public class Organization {
+	
+	private String id;
+	private String title;
+	private String description;
+	private String image;
+	private String name;
+	
+	public Organization(String id, String title, String description, String image, String name) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.image = image;
+		this.name = name;
+	}
+
+	
+}
