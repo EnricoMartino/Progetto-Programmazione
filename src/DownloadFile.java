d6972c11e111b925a4e48eb0391f87c063d00de1
Luca Munerati <s1082299@studenti.univpm.it>
2019-06-05 12:23:03 +0200

    Add method main for JsonParse
    Implement method in main for Parse the Json with the method DownloadfromURL (String,String,int)

--- src/DownloadFile.java
+++ src/DownloadFile.java
@@ -1,14 +1,25 @@
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.ParseException;
 
 public class DownloadFile {
 
-	public void downloadFileFromURL(String url, String filePathName, 
+	
+	public static void downloadFileFromUrl(String url, String filePathName, 
 			int readTimeoutInSeconds) 
 			throws MalformedURLException, IOException{
 				
@@ -30,6 +41,54 @@ public class DownloadFile {
 			}
 			bout.close();
 			in.close();
-		}	
+		}
 	
+	public static void main(String args[]) {
+		
+		String url = "https://www.dati.gov.it/api/3/action/package_show?id=5f23b30c-a393-4891-b7f5-69b7732e446e";
+		if(args.length == 1)
+			url = args[0]; 
+		try {
+			
+			URLConnection openConnection = new URL(url).openConnection();
+			openConnection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
+			InputStream in = openConnection.getInputStream();
+			
+			 String data = "";
+			 String line = "";
+			 try {
+			   InputStreamReader inR = new InputStreamReader( in );
+			   BufferedReader buf = new BufferedReader( inR );
+			  
+			   while ( ( line = buf.readLine() ) != null ) {
+				   data+= line;
+				   System.out.println( line );
+			   }
+			 } finally {
+			   in.close();
+			 }
+			JSONObject obj = (JSONObject) JSONValue.parseWithException(data); 
+			JSONObject objI = (JSONObject) (obj.get("result"));
+			JSONArray objA = (JSONArray) (objI.get("resources"));
+			
+			for(Object o: objA){
+			    if ( o instanceof JSONObject ) {
+			        JSONObject o1 = (JSONObject)o; 
+			        String format = (String)o1.get("format");
+			        String urlD = (String)o1.get("url");
+			        System.out.println(format + " | " + urlD);
+			        if(format.equals("csv")) {
+			        	downloadFileFromUrl(urlD, "UnivPm.csv",1000);
+			        }
+			    }
+			}
+			System.out.println( "OK" );
+		} catch (IOException | ParseException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
+
+	
