22de0f1c6e1fee0683ec98a122f6c96778fd6f3a
MuneLuca98 <s1082299@studenti.univpm.it>
2019-06-14 19:13:47 +0200

    Add old files classes to new package

---
+++ src/com/example/Appartamento.java
@@ -0,0 +1,137 @@
+package com.example;
+import java.io.*;
+import java.util.List;
+import java.util.List.*;
+
+
+public class Appartamento implements Serializable {
+	String codice;
+	String ubicazione;
+	String area_di_competenza;
+	String descrizione_via;
+	String civico;
+	String codice_via;
+	String posti_abitativi;
+	String  posti_letto;
+	String municipio;
+	String longitudine;
+	String latitudine;
+	String location;
+	
+	
+	
+	//generate constructor of class "Appartamento" 
+	public Appartamento(String codice, String ubicazione, String area_di_competenza, String descrizione_via, String civico,
+			String codice_via, String posti_abitativi, String posti_letto, String municipio, String longitudine, String latitudine,
+			String location) {
+		this.codice = codice;
+		this.ubicazione = ubicazione;
+		this.area_di_competenza = area_di_competenza;
+		this.descrizione_via = descrizione_via;
+		this.civico = civico;
+		this.codice_via = codice_via;
+		this.posti_abitativi = posti_abitativi;
+		this.posti_letto = posti_letto;
+		this.municipio = municipio;
+		this.longitudine = longitudine;
+		this.latitudine = latitudine;
+		this.location = location;
+	}
+	
+	public Appartamento( final String[] home)  {
+		this(home[0],home[1],home[2],home[3],home[4],home[5],home[6],home[7],home[8],home[9],home[10],home[11]);
+	}
+	@Override
+	public String toString() {
+		return "Appartamento [codice=" + codice + ", ubicazione=" + ubicazione + ", area_di_competenza="
+				+ area_di_competenza + ", descrizione_via=" + descrizione_via + ", civico=" + civico + ", codice_via="
+				+ codice_via + ", posti_abitativi=" + posti_abitativi + ", posti_letto=" + posti_letto + ", municipio="
+				+ municipio + ", longitudine=" + longitudine + ", latitudine=" + latitudine + ", location=" + location
+				+ "]";
+	}
+	//generate getters and setters of class "Appartamento"
+	public String getCodice() {
+		return codice;
+	}
+
+	public void setCodice(String codice) {
+		this.codice = codice;
+	}
+
+	public String getUbicazione() {
+		return ubicazione;
+	}
+
+	public void setUbicazione(String ubicazione) {
+		this.ubicazione = ubicazione;
+	}
+
+	public String getArea_di_competenza() {
+		return area_di_competenza;
+	}
+
+	public void setArea_di_competenza(String area_di_competenza) {
+		this.area_di_competenza = area_di_competenza;
+	}
+
+	public String getDescrizione_via() {
+		return descrizione_via;
+	}
+
+	public void setDescrizione_via(String descrizione_via) {
+		this.descrizione_via = descrizione_via;
+	}
+
+	public String getCivico() {
+		return civico;
+	}
+
+	public void setCivico(String civico) {
+		this.civico = civico;
+	}
+
+	public String getCodice_via() {
+		return codice_via;
+	}
+
+	public void setCodice_via(String codice_via) {
+		this.codice_via = codice_via;
+	}
+	public String getPosti_abitativi() {
+		return posti_abitativi;
+	}
+	public void setPosti_abitativi(String posti_abitativi) {
+		this.posti_abitativi = posti_abitativi;
+	}
+	public String getPosti_letto() {
+		return posti_letto;
+	}
+	public void setPosti_letto(String posti_letto) {
+		this.posti_letto = posti_letto;
+	}
+	public String getMunicipio() {
+		return municipio;
+	}
+	public void setMunicipio(String municipio) {
+		this.municipio = municipio;
+	}
+	public String getLongitudine() {
+		return longitudine;
+	}
+	public void setLongitudine(String longitudine) {
+		this.longitudine = longitudine;
+	}
+	public String getLatitudine() {
+		return latitudine;
+	}
+	public void setLatitudine(String latitudine) {
+		this.latitudine = latitudine;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+}
---
+++ src/com/example/DownloadFile.java
@@ -0,0 +1,104 @@
+package com.example;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Vector;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.ParseException;
+
+public class DownloadFile {
+
+	public static void downloadFileFromUrl(String url, String filePathName, int readTimeoutInSeconds)
+			throws MalformedURLException, IOException {
+
+		// create connection, setting a timeout
+		URLConnection urlConn = new URL(url).openConnection();
+		if (readTimeoutInSeconds > 0)
+			urlConn.setReadTimeout(readTimeoutInSeconds * 1000);
+
+		// create the stream
+		BufferedInputStream in = new BufferedInputStream(urlConn.getInputStream());
+		FileOutputStream fos = new FileOutputStream(filePathName);
+
+		// read the file
+		BufferedOutputStream bout = new BufferedOutputStream(fos, 1024);
+		byte[] data = new byte[1024];
+		int x = 0;
+		while ((x = in.read(data, 0, 1024)) >= 0) {
+			bout.write(data, 0, x);
+		}
+		bout.close();
+		in.close();
+	}
+
+	public static void main(String args[]) {
+
+		String url = "https://www.dati.gov.it/api/3/action/package_show?id=5f23b30c-a393-4891-b7f5-69b7732e446e";
+		if (args.length == 1)
+			url = args[0];
+		try {
+
+			URLConnection openConnection = new URL(url).openConnection();
+			openConnection.addRequestProperty("User-Agent",
+					"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
+			InputStream in = openConnection.getInputStream();
+
+			String data = "";
+			String line = "";
+			try {
+				InputStreamReader inR = new InputStreamReader(in);
+				BufferedReader buf = new BufferedReader(inR);
+
+				while ((line = buf.readLine()) != null) {
+					data += line;
+					System.out.println(line);
+				}
+			} finally {
+				in.close();
+			}
+			JSONObject obj = (JSONObject) JSONValue.parseWithException(data);
+			JSONObject objI = (JSONObject) (obj.get("result"));
+			JSONArray objA = (JSONArray) (objI.get("resources"));
+
+			for (Object o : objA) {
+				if (o instanceof JSONObject) {
+					JSONObject o1 = (JSONObject) o;
+					String format = (String) o1.get("format");
+					String urlD = (String) o1.get("url");
+					System.out.println(format + " | " + urlD);
+					if (format.equals("csv")) {
+						downloadFileFromUrl(urlD, "UnivPm.csv", 1000);
+					}
+				}
+			}
+			System.out.println("OK");
+		} catch (IOException | ParseException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
