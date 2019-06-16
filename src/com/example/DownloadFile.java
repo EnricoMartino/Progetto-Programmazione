51c7bf3d6f4d24675b59fa483babb93070e93a4f
MuneLuca98 <s1082299@studenti.univpm.it>
2019-06-16 19:18:59 +0200

    Update DownloadFile
    Fix the warnings, removing the import not used

--- src/com/example/DownloadFile.java
+++ src/com/example/DownloadFile.java
@@ -2,25 +2,14 @@ package com.example;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Vector;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
