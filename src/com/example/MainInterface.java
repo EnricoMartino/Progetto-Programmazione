ed00450a0cb30c21ae71c18f2bc765d9504bb78a
MuneLuca98 <s1082299@studenti.univpm.it>
2019-06-19 19:28:58 +0200

    Create MainInterface.java
    Here we implement the menu for a correct use of program

---
+++ src/com/example/MainInterface.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class MainInterface {
+
+	DownloadFile download = new DownloadFile();
+	Serialization ser = new Serialization();
+	Data data = new Data();
+	Metadata metadata = new Metadata();
+	
+	
+	
+}
