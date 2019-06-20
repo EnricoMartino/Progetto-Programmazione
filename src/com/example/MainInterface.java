6e7a75b2234b5cbe1e580ceb0c18a002e42e1062
MuneLuca98 <s1082299@studenti.univpm.it>
2019-06-20 18:20:39 +0200

    Update MainInterface
    Check if the file already exist

--- src/com/example/MainInterface.java
+++ src/com/example/MainInterface.java
@@ -1,31 +1,43 @@
 package com.example;
-//import java libraries
+
 import java.awt.List;
+import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class MainInterface {
-   // create main of the program
+
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
-	DownloadFile download = new DownloadFile();  //create an object called "download" of "DownloadFile"
-	Serialization ser = new Serialization();   //create an object called "ser" of "Serialization"
-	Data data = new Data();//create an object called "data" of "Data"
-	Metadata metadata = new Metadata();//create an object called "metadata" of "Metadata"
-	Scanner on = new Scanner(System.in);  //create an object called "on" of "Scanner"
-	int scelta = 0;                //initializes an int "scelta" to 0
-	boolean select=false;          //initializes a boolean "select" to false
-	//print message of welcome and give instructions on the program
-	System.out.println("Benvenuto nel Programma di Case Vacanze di Milano\n");
-	System.out.println("Per funzionare questo programma deve scaricare un file che noi chiameremo \"UnivPm.csv\"");
-	System.out.println("Dopo il download, il programma far√† un lavoro di serializzazione per salvare i dati");
-	System.out.println("Quindi partiamo, adesso eseguo il tutto e ti chieder√≤ alcune cose. \n");
+	DownloadFile download = new DownloadFile();
+	Serialization ser = new Serialization();
+	Data data = new Data();
+	Metadata metadata = new Metadata();
+	Scanner on = new Scanner(System.in);
+	int scelta = 0;
+	boolean select=false;
+	final String csvfile = "UnivPm.csv";
 	
-	download.main(args); //call the method download and start the download of the file
-	ser.serialize();     //call the method ser and start the serialization
+	File exists = new File(csvfile);
+	boolean exit = exists.exists();
+	if(exit==true) {
+		System.out.println("Benvenuto nel Programma di Case Vacanze di Milano\n");
+		System.out.println("Per funzionare questo programma deve scaricare un file che noi chiameremo \"UnivPm.csv\"");
+		System.out.println("Dopo essere scaricato, il programma far‡ un lavoro di serializzazione per salvare i dati");
+		System.out.println("Quindi partiamo, adesso eseguo il tutto e ti chiedero alcune cose.");
+		System.out.println("Il file esiste gi‡, ma visto che hai fatto partire il programma, proseguiamo.\n");
 	
+	}else {
+		System.out.println("Benvenuto nel Programma di Case Vacanze di Milano\n");
+		System.out.println("Per funzionare questo programma deve scaricare un file che noi chiameremo \"UnivPm.csv\"");
+		System.out.println("Dopo essere scaricato, il programma far‡ un lavoro di serializzazione per salvare i dati");
+		System.out.println("Quindi partiamo, adesso eseguo il tutto e ti chiedero alcune cose. \n");
+		download.main(args);
+		ser.serialize();	
+	}
+
 	System.out.println("Bene adesso abbiamo tutti i dati, possiamo elobolarli");
 	System.out.println("Qua sotto ti metto delle opzioni!!\n");
-	//create a menu to allow to choose what to do
+	
 	do {
 	System.out.println("1-Serializzare i dati su di un file .ser");
 	System.out.println("2-Creare un file .json dove verrano salvati i Metadata");
@@ -33,13 +45,13 @@ public class MainInterface {
 	System.out.println("4-Creare un file .json dove verranno salvati i Data utilizzando un filtro scelto da te");
 	System.out.println("5-Creare un file .json dove verranno salvati delle statistiche sui valori numerici in base alla tua scelta");
 	System.out.println("0-Exit");
-	scelta=on.nextInt();   //allow to write a number from 1 to 5 to do various operations(0 to exit)
+	scelta=on.nextInt();
 	switch(scelta) {
 	case 1:
 		ser.outputfile(ser.lista);
 		break;
 	case 2:
-		metadata.toJSonMetadata();;
+		metadata.toJsonMedataWithObject();
 		break;
 	case 3:
 		data.toJsonData();
